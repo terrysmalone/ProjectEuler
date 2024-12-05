8090d9386275644a523f114175c556e944efdf62
Terry Malone <terrysmalone@hotmail.com>
2024-12-05 15:01:06 +0000

    Add problem 3

---
+++ ProjectEuler/Problem3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler;
+internal sealed class Problem3
+{
+    internal ulong GetLargestPrimeFactors(ulong num)
+    {
+        if (num == 0)
+        {
+            return num;
+        }
+
+        bool factored = false;
+
+        List<ulong> primeFactors = new List<ulong>();
+        ulong current = num;
+
+        while (!factored)
+        {
+            if (current % 2 == 0)
+            {
+                primeFactors.Add(2);
+                current /= 2;
+                continue;
+            } 
+            else
+            {
+                for (ulong i = 3; i < Math.Sqrt(current); i+=2)
+                {
+                    if (current % i == 0)
+                    {
+                        primeFactors.Add(i);
+                        current /= i;
+                        continue;
+                    }
+                }
+            }
+
+            primeFactors.Add(current);
+
+            factored = true;
+        }
+
+        return primeFactors.Max();
+    }
+}
---
+++ ProjectEulerTests/Problem3Tests.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+using System.Reflection.Metadata;
+using ProjectEuler;
+namespace ProjectEulerTests;
+
+public class Problem3Tests
+{
+    [TestCase((ulong)0, (ulong)0)]
+    [TestCase((ulong)1, (ulong)1)]
+    [TestCase((ulong)2, (ulong)2)]
+    [TestCase((ulong)3, (ulong)3)]
+    [TestCase((ulong)4, (ulong)2)]
+    [TestCase((ulong)5, (ulong)5)]
+    [TestCase((ulong)10, (ulong)5)]
+    [TestCase((ulong)13195, (ulong)29)]
+    public void TestProblem(ulong number, ulong expected)
+    {
+        Problem3 problem = new Problem3();
+        ulong result = problem.GetLargestPrimeFactors(number);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestSolution()
+    {
+        Problem3 problem = new Problem3();
+        ulong result = problem.GetLargestPrimeFactors(600851475143);
+
+        Assert.That(result, Is.EqualTo(6857));
+    }
+}

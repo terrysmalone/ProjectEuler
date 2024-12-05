0b86e78d33ecf8d2fdaf30f4030aeba5d5940ac8
Terry Malone <terrysmalone@hotmail.com>
2024-12-05 16:35:40 +0000

    Add problem 4

---
+++ ProjectEuler/Problem4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler;
+internal sealed class Problem4
+{
+    internal int GetLargestPalindromeProduct(int min, int max)
+    {
+        // Lets just brute force it
+        int largestPalindrome = 0;
+        for (int i = max; i >= min; i--)
+        {
+            for (int j = max; j >= min; j--)
+            {
+                int product = i * j;
+
+                if (IsPalindrome(product))
+                {
+                    if (product > largestPalindrome)
+                    {
+                        largestPalindrome = product;
+                    }
+                }
+            }
+        }
+
+        return largestPalindrome;
+    }
+
+    private static bool IsPalindrome(int number)
+    {
+        string numAsString = number.ToString();
+
+        char[] numArray = numAsString.ToCharArray();
+        Array.Reverse(numArray);
+        string reversed = new string(numArray);
+
+        if (numAsString == reversed)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ ProjectEulerTests/Problem4Tests.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+using System.Reflection.Metadata;
+using ProjectEuler;
+namespace ProjectEulerTests;
+
+public class Problem4Tests
+{
+    [TestCase(0, 10, 0)]
+    [TestCase(0, 11, 121)]
+    [TestCase(10, 99, 9009)]
+    public void TestProblem(int min, int max, int expected)
+    {
+        Problem4 problem = new Problem4();
+        int result = problem.GetLargestPalindromeProduct(min, max);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestSolution()
+    {
+        Problem4 problem = new Problem4();
+        int result = problem.GetLargestPalindromeProduct(100, 999);
+
+        Assert.That(result, Is.EqualTo(906609));
+    }
+}

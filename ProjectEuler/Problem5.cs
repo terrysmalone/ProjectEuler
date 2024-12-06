fb8bcdcdb77b857a96902957deadf2813a76210a
Terry Malone <terrysmalone@hotmail.com>
2024-12-06 16:28:47 +0000

    Problem 5

---
+++ ProjectEuler/Problem5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler;
+internal sealed class Problem5
+{
+    internal int GetSmallestMultiple(int min, int max)
+    {
+        int current = 0;
+        bool found = false;
+
+        while (!found)
+        {
+            current += max;
+            found = true;
+
+            for (int i = min; i < max; i++)
+            {
+                if (current % i != 0)
+                {
+                    found = false;
+                    break;
+                }
+            }
+        }
+
+        return current;
+    }
+}
---
+++ ProjectEulerTests/Problem5Tests.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+using System.Reflection.Metadata;
+using ProjectEuler;
+namespace ProjectEulerTests;
+
+public class Problem5Tests
+{
+    [TestCase(1, 1, 1)]
+    [TestCase(1, 3, 6)]
+    [TestCase(1, 10, 2520)]
+    public void TestProblem(int min, int max, int expected)
+    {
+        Problem5 problem = new Problem5();
+        int result = problem.GetSmallestMultiple(min, max);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestSolution()
+    {
+        Problem5 problem = new Problem5();
+        int result = problem.GetSmallestMultiple(1, 20);
+
+        Assert.That(result, Is.EqualTo(232792560));
+    }
+}

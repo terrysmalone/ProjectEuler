3cee91f9a6ab9cf61d27f56dc4b0a033c540ea42
Terry Malone <terrysmalone@hotmail.com>
2024-12-06 16:28:56 +0000

    Problem 6

---
+++ ProjectEuler/Problem6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler;
+internal sealed class Problem6
+{
+    internal int GetSumSquareDifference(int min, int max)
+    {
+        // To calculate the difference between the sum of squares
+        // and the square of sums of a range of numbers we could just
+        // brute force it with for loops. But.....
+        //
+        // (a + b)^2 = a^2 + b^2 + 2ab
+        // so the difference between the two is 2ab
+        // or the difference is equal to the sum of all double products
+        // So....
+        int total = 0;
+
+        for (int a = min; a < max; a++)
+        {
+            for (int b = a+1; b <= max; b++)
+            {
+                if (a != b)
+                {
+                    total += 2 * a * b;
+                }
+            }
+        }
+
+        return total;
+    }
+}
---
+++ ProjectEulerTests/Problem6Tests.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using System.Reflection.Metadata;
+using ProjectEuler;
+namespace ProjectEulerTests;
+
+public class Problem6Tests
+{
+    [TestCase(1, 10, 2640)]
+    public void TestProblem(int min, int max, int expected)
+    {
+        Problem6 problem = new Problem6();
+        int result = problem.GetSumSquareDifference(min, max);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestSolution()
+    {
+        Problem6 problem = new Problem6();
+        int result = problem.GetSumSquareDifference(1, 100);
+
+        Assert.That(result, Is.EqualTo(25164150));
+    }
+}

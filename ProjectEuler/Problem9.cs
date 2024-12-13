39fb508c35f1437a6d7922ea77f92d14bcbb3920
Terry Malone <terrysmalone@hotmail.com>
2024-12-13 22:52:57 +0000

    Problem 9. Brute force approach

---
+++ ProjectEuler/Problem9.cs
@@ -0,0 +1,50 @@
+﻿using ProjectEuler.Libraries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler;
+internal sealed class Problem9
+{
+    internal int GetPythagoreanTripletProduct(int num)
+    {
+        // Lets start by brute forcing it. We can get 
+        // more elegant as needed.
+
+        // Assumption:
+        // There is only one pythagorean triplet of num.
+        // It'll just return the first match
+
+        // Rules:
+        // a < b < c
+        // a^2 + b^2 = c^2
+        // a + b + c = num 
+        for (int c = num - 3; c >= 3; c--)
+        {
+            for (int b = c - 1; b >= 2; b--)
+            {
+                if (c + b >= num)
+                {
+                    continue;
+                }
+
+                for (int a = b - 1; a >= 1; a--)
+                {
+                    if (a + b + c != num)
+                    {
+                        continue;
+                    }
+
+                    if ((a * a) + (b * b) == c * c)
+                    {
+                        return a * b * c;
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ ProjectEulerTests/Problem9Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+using System.Reflection.Metadata;
+using ProjectEuler;
+namespace ProjectEulerTests;
+
+public class Problem9Tests
+{
+    [TestCase(12, 60)]
+    public void TestProblem(int num, int expected)
+    {
+        Problem9 problem = new Problem9();
+        int result = problem.GetPythagoreanTripletProduct(12);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestSolution()
+    {
+        Problem9 problem = new Problem9();
+        int result = problem.GetPythagoreanTripletProduct(1000);
+
+        Assert.That(result, Is.EqualTo(31875000));
+    }
+}

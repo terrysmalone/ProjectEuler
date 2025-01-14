fdd7be14399b84b751f904c015e07dd6533e49b3
Terry Malone <terrysmalone@hotmail.com>
2025-01-14 23:19:06 +0000

    Solve problem 11

---
+++ ProjectEulerTests/Problem11Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+using System.Reflection.Metadata;
+using ProjectEuler;
+namespace ProjectEulerTests;
+
+public class Problem11Tests
+{
+    [Test]
+    public void TestSolution()
+    {
+        var problem = new Problem11();
+        int result = problem.GetLargestProduct();
+
+        Assert.That(result, Is.EqualTo(70600674));
+    }
+}

74e3c40811b7c09101de81ae6d0ac34e9c054ec1
Terry Malone <terrysmalone@hotmail.com>
2025-01-15 14:53:11 +0000

    Problem 13

---
+++ ProjectEulerTests/Problem13Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+using System.Reflection.Metadata;
+using ProjectEuler;
+namespace ProjectEulerTests;
+
+public class Problem13Tests
+{
+    [Test]
+    public void TestSolution()
+    {
+        var problem = new Problem13();
+        ulong result = problem.GetLargeSum(10);
+
+        Assert.That(result, Is.EqualTo(5537376230));
+    }
+}

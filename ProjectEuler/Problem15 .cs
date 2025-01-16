c7795bcc3e77493503c39843286a4f21cf584970
Terry Malone <terrysmalone@hotmail.com>
2025-01-16 15:31:41 +0000

    Solve problem 15

---
+++ ProjectEuler/Problem15 .cs
@@ -0,0 +1,26 @@
+﻿using ProjectEuler.Libraries;
+using System.Collections;
+using System.Numerics;
+
+namespace ProjectEuler;
+internal sealed class Problem15
+{
+    // We can get the lattice path count using the factorials of x and y
+    // = (x + y)! / x! . y!
+    internal BigInteger GetLatticePathLength(int x, int y)
+    {
+        return CalculateFactorial(x + y) / (CalculateFactorial(x) * CalculateFactorial(y));
+    }
+
+    private BigInteger CalculateFactorial(int num)
+    {
+        BigInteger result = 1;
+
+        for (var i = num; i >= 1; i--)
+        {
+            result *= i;
+        }
+
+        return result;
+    }
+}
---
+++ ProjectEulerTests/Problem15Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+using System.Reflection.Metadata;
+using ProjectEuler;
+namespace ProjectEulerTests;
+
+public class Problem15Tests
+{
+    [TestCase(1, 1, 2)]
+    [TestCase(2, 2, 6)]
+    [TestCase(2, 4, 15)]
+    public void TestProblem(int x, int y, int expected)
+    {
+        var problem = new Problem15();
+        BigInteger result = problem.GetLatticePathLength(x, y);
+
+        Assert.That(result, Is.EqualTo(new BigInteger(expected)));
+    }
+
+    [Test]
+    public void TestSolution()
+    {
+        var problem = new Problem15();
+        BigInteger result = problem.GetLatticePathLength(20, 20);
+
+        Assert.That(result, Is.EqualTo(new BigInteger(137846528820)));
+    }
+}
